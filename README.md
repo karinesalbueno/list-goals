93665f89706bae041d7a9ddba16b0eee4f6799bc
karinesalbueno <karinebsalbueno@gmail.com>
2022-09-27 10:01:35 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+#### Using styled components library to style 💅
+
+###`npm install`
+first run for install all dependencies
+
+### `npm start`
+
+Runs the app in the development mode.\
+Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
---
+++ src/App.js
@@ -0,0 +1,57 @@
+import React, { useState } from 'react';
+
+import GoalList from './components/Goals/GoalList/GoalList';
+import Input from './components/Goals/Input/Input';
+import './App.css';
+
+const App = () => {
+  const [Goals, setGoals] = useState([
+    { text: 'Do all exercises!', id: '1' },
+    { text: 'Finish course jest', id: '2' }
+  ]);
+
+  const addGoalHandler = enteredText => {
+    setGoals(prevGoals => {
+      const updatedGoals = [...prevGoals];
+      updatedGoals.unshift({ text: enteredText, id: Math.random().toString() });
+      return updatedGoals;
+    });
+  };
+
+  const deleteItemHandler = goalId => {
+    setGoals(prevGoals => {
+      const updatedGoals = prevGoals.filter(goal => goal.id !== goalId);
+      return updatedGoals;
+    });
+  };
+
+  let content = (
+    <p style={{ textAlign: 'center' }}>No goals found. Maybe add one?</p>
+  );
+
+  if (Goals.length > 0) {
+    content = (
+      <GoalList items={Goals} onDeleteItem={deleteItemHandler} />
+    );
+  }
+
+  return (
+    <div>
+      <section id="goal-form">
+        <Input onAddGoal={addGoalHandler} />
+      </section>
+      <section id="goals">
+        {content}
+        {/* {Goals.length > 0 && (
+          <GoalList
+            items={Goals}
+            onDeleteItem={deleteItemHandler}
+          />
+        ) // <p style={{ textAlign: 'center' }}>No goals found. Maybe add one?</p>
+        } */}
+      </section>
+    </div>
+  );
+};
+
+export default App;
