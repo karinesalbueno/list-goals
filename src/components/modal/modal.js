3bde299be0cda7dea500769652fb9814fc3158c5
karinesalbueno <karinebsalbueno@gmail.com>
2022-09-29 20:03:25 -0300

    background modal

--- src/components/modal/modal.js
+++ src/components/modal/modal.js
@@ -4,12 +4,13 @@ import './modal.css'
 const Modal = (props) => {
 
     return (
-      
+      <div className="background">
         <div className="modalContainer">
             <h2>Poxa, que pena...</h2>
             <p>{props.message}</p>
             <button className="buttonModal" onClick={props.onClick}>Ok 👍  </button>
        
+        </div>
         </div>
     )
 
